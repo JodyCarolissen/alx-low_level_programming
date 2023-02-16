2fc218e3f604056205bbb1fd2a4e01b7e25cf583
JodyCarolissen <jodycarolissen28@gmail.com>
2023-02-16 14:36:10 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints "Programming is like building a multilingual puzzle, new line"
- * return to 0.
- * /
- 
+ * return Always 0
+ * */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 }
