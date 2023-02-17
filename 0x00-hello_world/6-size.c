2adb39e60fd0b6a062f511b11f02d485d584aa5c
JodyCarolissen <jodycarolissen28@gmail.com>
2023-02-16 16:59:50 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main entry - Entry point
+ *  
+ *  Return: Always 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	float c;
+
+	printf("Size of char: %lu byte(s)\", (unsigned long)sizeof(a));
+	printf("Size of int: %lu byte(s)\", (unsigned long)sizeof(b));
+	printf("Size of float: %lu byte(s)\", (unsigned long)sizeof(c));
+	return (0);
+}
+
+
