74bd10b2c39333a1cf971487635ae21aa052190f
JodyCarolissen <jodycarolissen28@gmail.com>
2023-03-08 09:23:45 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib>
+
 /*                                                                                                                                      
  * File: 0-puts_recursion.c                                                                                                                            
  */                                                                                                                                     
